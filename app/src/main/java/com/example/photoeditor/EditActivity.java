7a7ce212b5aab99c3635ebcef2b9afae32bb69e5
Zahra Fazel <zfazel1379@gmail.com>
2020-07-11 22:01:40 +0430

    complete start menu

---
+++ app/src/main/java/com/example/photoeditor/EditActivity.java
@@ -0,0 +1,97 @@
+package com.example.photoeditor;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.IOException;
+
+
+public class EditActivity extends AppCompatActivity
+{
+    private ImageView image;
+    private RecyclerView menu;
+    private RecyclerView submenu;
+    private Uri imageUri;
+    private Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+        image = findViewById(R.id.image_holder);
+        menu = findViewById(R.id.menu);
+        submenu = findViewById(R.id.submenu);
+        choosePhoto();
+    }
+
+    private void choosePhoto()
+    {
+        Intent intent = getIntent();
+        String method = intent.getStringExtra("method");
+        if( method.equals("pick from gallery") )
+        {
+            Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+            startActivityForResult(gallery, 2);
+        }
+        else if( method.equals("take photo") )
+        {
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.TITLE, "New Picture");
+            values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+            imageUri = getContentResolver().insert(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+                startActivityForResult(takePictureIntent, 1);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK)
+        {
+            if (requestCode == 1)
+            {
+                try
+                {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                assert bitmap != null;
+                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888 , true);
+                image.setImageURI(imageUri);
+
+            }
+            else if (requestCode == 2)
+            {
+                imageUri = data.getData();
+                try
+                {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+                assert bitmap != null;
+                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888 , true);
+                image.setImageURI(imageUri);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/photoeditor/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.photoeditor;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView gallery = findViewById(R.id.gallery);
+        ImageView camera = findViewById(R.id.camera);
+        ImageView collage = findViewById(R.id.collage);
+
+        gallery.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                displayEditActivity("pick from gallery");
+            }
+        });
+
+        camera.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                displayEditActivity("take photo");
+            }
+        });
+    }
+
+
+    public void displayEditActivity(String method)
+    {
+        Intent intent = new Intent(MainActivity.this, EditActivity.class);
+        intent.putExtra("method", method);
+        startActivity(intent);
+    }
+}
