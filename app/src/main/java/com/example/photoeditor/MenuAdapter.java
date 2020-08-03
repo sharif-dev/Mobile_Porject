1dc091f51492eb98b02f76bd39bf9e61cfdc10e9
Zahra Fazel <zfazel1379@gmail.com>
2020-08-03 23:19:05 +0430

    refactor main menu add new edit tools

--- app/src/main/java/com/example/photoeditor/MenuAdapter.java
+++ app/src/main/java/com/example/photoeditor/MenuAdapter.java
@@ -1,55 +1,97 @@
 package com.example.photoeditor;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 
-
-public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MyView>
+public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.ViewHolder>
 {
-    private String[] iconText;
-    private int[] icon;
 
-    public class MyView extends RecyclerView.ViewHolder
+    private List<ToolModel> toolList = new ArrayList<>();
+    private OnItemSelected onItemSelected;
+
+    public MenuAdapter(OnItemSelected onItemSelected)
     {
-        TextView iconText;
-        ImageView icon;
+        this.onItemSelected = onItemSelected;
+        toolList.add(new ToolModel("Adjust", R.drawable.adjust, ToolType.ADJUST));
+        toolList.add(new ToolModel("Crop", R.drawable.crop, ToolType.CROP));
+        toolList.add(new ToolModel("Filter", R.drawable.filter, ToolType.FILTER));
+        toolList.add(new ToolModel("Rotate", R.drawable.rotate, ToolType.ROTATE));
+        toolList.add(new ToolModel("Brush", R.drawable.brush, ToolType.BRUSH));
+        toolList.add(new ToolModel("Text", R.drawable.text, ToolType.TEXT));
+        toolList.add(new ToolModel("Frame", R.drawable.frame, ToolType.FRAME));
+        toolList.add(new ToolModel("Blur", R.drawable.blur, ToolType.BLUR));
+        toolList.add(new ToolModel("Eraser", R.drawable.eraser, ToolType.ERASER));
+        toolList.add(new ToolModel("Emoji", R.drawable.emoji, ToolType.EMOJI));
+    }
 
-        public MyView(View view)
+    public interface OnItemSelected
+    {
+        void onToolSelected(ToolType toolType);
+    }
+
+    class ToolModel
+    {
+        private String toolName;
+        private int toolIcon;
+        private ToolType toolType;
+
+        ToolModel(String toolName, int toolIcon, ToolType toolType)
         {
-            super(view);
-            iconText = (TextView)view.findViewById(R.id.icon_name);
-            icon = (ImageView)view.findViewById(R.id.icon_image);
+            this.toolName = toolName;
+            this.toolIcon = toolIcon;
+            this.toolType = toolType;
         }
+
     }
 
-    public MenuAdapter(String[] iconText, int[] icon)
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
-        this.icon = icon;
-        this.iconText = iconText;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_editing_tools, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public MyView onCreateViewHolder(ViewGroup parent, int viewType)
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position)
     {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent,  false);
-        return new MyView(itemView);
-    }
-
-    @Override
-    public void onBindViewHolder(final MyView holder, final int position)
-    {
-        holder.iconText.setText(iconText[position]);
-        holder.icon.setImageResource(icon[position]);
+        ToolModel item = toolList.get(position);
+        holder.txtTool.setText(item.toolName);
+        holder.imgToolIcon.setImageResource(item.toolIcon);
     }
 
     @Override
     public int getItemCount()
     {
-        return icon.length;
+        return toolList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder
+    {
+        ImageView imgToolIcon;
+        TextView txtTool;
+
+        ViewHolder(View itemView)
+        {
+            super(itemView);
+            imgToolIcon = itemView.findViewById(R.id.imgToolIcon);
+            txtTool = itemView.findViewById(R.id.txtTool);
+            itemView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    onItemSelected.onToolSelected(toolList.get(getLayoutPosition()).toolType);
+                }
+            });
+        }
     }
 }
---
+++ app/src/main/java/com/example/photoeditor/ToolType.java
@@ -0,0 +1,16 @@
+package com.example.photoeditor;
+
+public enum ToolType
+{
+    BRUSH,
+    TEXT,
+    ERASER,
+    FILTER,
+    EMOJI,
+    STICKER,
+    CROP,
+    ROTATE,
+    FRAME,
+    BLUR,
+    ADJUST
+}
