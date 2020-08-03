756d0dc093ebfc08f600173262063767624ef0ef
Zahra Fazel <zfazel1379@gmail.com>
2020-08-04 01:26:04 +0430

    add onItemSelected to EditActivity move choosePhoto from EditActivity to MainActivity

--- app/src/main/java/com/example/photoeditor/EditActivity.java
+++ app/src/main/java/com/example/photoeditor/EditActivity.java
@@ -3,18 +3,20 @@ package com.example.photoeditor;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.IOException;
 
 
-public class EditActivity extends AppCompatActivity
+public class EditActivity extends AppCompatActivity implements MenuAdapter.OnItemSelected
 {
     private ImageView image;
     private RecyclerView menu;
@@ -24,8 +26,7 @@ public class EditActivity extends AppCompatActivity
     private ImageView redo;
     private Uri imageUri;
     private Bitmap bitmap;
-
-    private Controller controller = Controller.getInstance();
+    private MenuAdapter menuAdapter = new MenuAdapter(this);
 
 
     @Override
@@ -44,72 +45,43 @@ public class EditActivity extends AppCompatActivity
         undo = findViewById(R.id.undo_icon);
         redo = findViewById(R.id.redo_icon);
         ok = findViewById(R.id.checked_icon);
-
-        choosePhoto();
-        controller.makeMenu(menu, EditActivity.this);
-        controller.controlPanel(cancel, undo, redo, ok, EditActivity.this);
+        imageUri = Uri.parse(getIntent().getStringExtra("uri"));
+        byte[] byteArray = getIntent().getByteArrayExtra("bitmap");
+        bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        image.setImageURI(imageUri);
+        LinearLayoutManager toolsLinearLayoutManager =
+                new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        menu.setLayoutManager(toolsLinearLayoutManager);
+        menu.setAdapter(menuAdapter);
     }
 
-    private void choosePhoto()
-    {
-        Intent intent = getIntent();
-        String method = intent.getStringExtra("method");
-        if( method.equals("pick from gallery") )
-        {
-            Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-            startActivityForResult(gallery, 2);
-        }
-        else if( method.equals("take photo") )
-        {
-            ContentValues values = new ContentValues();
-            values.put(MediaStore.Images.Media.TITLE, "New Picture");
-            values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
-            imageUri = getContentResolver().insert(
-                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null)
-                startActivityForResult(takePictureIntent, 1);
-        }
-    }
+
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    public void onToolSelected(ToolType toolType)
     {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK)
+        switch (toolType)
         {
-            if (requestCode == 1)
-            {
-                try
-                {
-                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
-                }
-                catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
-                assert bitmap != null;
-                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888 , true);
-                image.setImageURI(imageUri);
-
-            }
-            else if (requestCode == 2)
-            {
-                imageUri = data.getData();
-                try
-                {
-                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-                assert bitmap != null;
-                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888 , true);
-                image.setImageURI(imageUri);
-            }
+            case ADJUST:
+                break;
+            case CROP:
+                break;
+            case FILTER:
+                break;
+            case ROTATE:
+                break;
+            case BRUSH:
+                break;
+            case TEXT:
+                break;
+            case FRAME:
+                break;
+            case BLUR:
+                break;
+            case ERASER:
+                break;
+            case EMOJI:
+                break;
         }
     }
-
 }
--- app/src/main/java/com/example/photoeditor/MainActivity.java
+++ app/src/main/java/com/example/photoeditor/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.photoeditor;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private Uri imageUri;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -31,7 +42,8 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                displayEditActivity("pick from gallery");
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(gallery, 2);
             }
         });
 
@@ -40,16 +52,65 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                displayEditActivity("take photo");
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.TITLE, "New Picture");
+                values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+                imageUri = getContentResolver().insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+                    startActivityForResult(takePictureIntent, 1);
             }
         });
     }
 
-
-    public void displayEditActivity(String method)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK)
+        {
+            if (requestCode == 1)
+            {
+                try
+                {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                assert bitmap != null;
+                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888 , true);
+                displayEditActivity();
+            }
+            else if (requestCode == 2)
+            {
+                imageUri = data.getData();
+                try
+                {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+                assert bitmap != null;
+                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888 , true);
+                displayEditActivity();
+            }
+        }
+    }
+
+    public void displayEditActivity()
+    {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
         Intent intent = new Intent(MainActivity.this, EditActivity.class);
-        intent.putExtra("method", method);
+        intent.putExtra("bitmap", byteArray);
+        intent.putExtra("uri", imageUri.toString());
         startActivity(intent);
     }
 }
